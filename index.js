19ede0b4e4388c48b541ee0ac474ca6a1c8dcca4
Marshall Thompson <marshall@creativeideal.net>
2017-03-24 12:27:29 -0600

    Add pbkdf2 and create-hmac

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var bip39 = require('bip39');
+var pbkdf2 = require('pbkdf2');
+var createHmac = require('create-hmac');
 
 module.exports = {
-  bip39: bip39
+  bip39: bip39,
+  pbkdf2: pbkdf2,
+  createHmac: createHmac
 };
