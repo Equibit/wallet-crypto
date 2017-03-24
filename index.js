7521881da4961b703814b7390a81a57f6cf76457
Marshall Thompson <marshall@creativeideal.net>
2017-03-24 14:46:01 -0600

    Add create-hash package.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var bip39 = require('bip39');
 var pbkdf2 = require('pbkdf2');
 var createHmac = require('create-hmac');
+var createHash = require('create-hash');
 
 module.exports = {
   bip39: bip39,
   pbkdf2: pbkdf2.pbkdf2,
   pbkdf2Sync: pbkdf2.pbkdf2Sync,
-  createHmac: createHmac
+  createHmac: createHmac,
+  createHash: createHash
 };
