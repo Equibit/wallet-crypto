3a5775aeef41a9c9a96b2372a1ab2ba109ee7370
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-03-28 18:19:29 -0400

    added bitcoinjs-lib

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var bip39 = require('bip39');
+var bitcoin = require('bitcoinjs-lib');
 var pbkdf2 = require('pbkdf2');
 var createHmac = require('create-hmac');
 var createHash = require('create-hash');
 
 module.exports = {
   bip39: bip39,
+  bitcoin: bitcoin,
   pbkdf2: pbkdf2.pbkdf2,
   pbkdf2Sync: pbkdf2.pbkdf2Sync,
   createHmac: createHmac,
