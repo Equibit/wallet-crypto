1277c4a205ce849904b63742bbbdb96476212b23
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-08-30 17:12:33 -0400

    added tx-builder-equibit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var bitcoin = require('bitcoinjs-lib');
 var pbkdf2 = require('pbkdf2');
 var createHmac = require('create-hmac');
 var createHash = require('create-hash');
+var eqbTxBuilder = require('tx-builder-equibit/src/tx-builder-equibit');
 
 module.exports = {
   bip39: bip39,
@@ -10,5 +11,6 @@ module.exports = {
   pbkdf2: pbkdf2.pbkdf2,
   pbkdf2Sync: pbkdf2.pbkdf2Sync,
   createHmac: createHmac,
-  createHash: createHash
+  createHash: createHash,
+  eqbTxBuilder: eqbTxBuilder
 };
